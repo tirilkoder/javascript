388377f3e6eef4486ab95ce9e3c6532d11bcb258
tirilkoder <tiril.koder@gmail.com>
2023-11-20 13:53:14 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+// Task one
+
+// Find and store a reference to the class "toggle_button" in the html document
+const button = document.querySelector(".toggle_button");
+
+function themeToggle() {
+    // Find and store a reference to the class "body" in the html document
+    const body = document.querySelector("body");
+
+    // Toggle the class "red_mode" on the class "body"
+    body.classList.toggle("christmas_mode");
+}    
+
+// Add an event listener to the div class, listening for a "click" event, and preforming the function when activated
+button.addEventListener("click", themeToggle);
