a037cd72566379222c45c8f9f14eb0f77be56d0e
tirilkoder <tiril.koder@gmail.com>
2023-11-21 10:09:43 +0100

    Added a script for the second button

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 // Task one
 
+// Button one
+
 // Find and store a reference to the class "toggle_button" in the html document
 const button = document.querySelector(".toggle_button");
 
@@ -13,3 +15,19 @@ function themeToggle() {
 
 // Add an event listener to the div class, listening for a "click" event, and preforming the function when activated
 button.addEventListener("click", themeToggle);
+
+//Button two
+
+// Find and store a reference to the class "toggle_button" in the html document
+const button_1 = document.querySelector(".toggle");
+
+function themeToggle() {
+    // Find and store a reference to the class "body" in the html document
+    const body = document.querySelector("body");
+
+    // Toggle the class "red_mode" on the class "body"
+    body.classList.toggle("christmas_mode");
+}    
+
+// Add an event listener to the div class, listening for a "click" event, and preforming the function when activated
+button_1.addEventListener("click", themeToggle);
